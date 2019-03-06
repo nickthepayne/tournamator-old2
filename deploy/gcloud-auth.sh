01dedf54de2815ab62c20108d7878ff53be09a6a
Nick Payne <nick.payne89@gmail.com>
2019-03-06 14:38:56 +0800

    deploy to gcloud from circleci

--- deploy/gcloud-auth.sh
+++ deploy/gcloud-auth.sh
@@ -4,5 +4,5 @@ set -e
 
 echo ${GCLOUD_SERVICE_KEY} > ${HOME}/gcp-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcp-key.json
-gcloud --quiet config set project ${GCLOUD_PROJECT_ID}
-gcloud config set compute/zone ${GCLOUD_COMPUTE_ZONE}
+gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
+gcloud config set compute/zone ${GOOGLE_COMPUTE_ZONE}
