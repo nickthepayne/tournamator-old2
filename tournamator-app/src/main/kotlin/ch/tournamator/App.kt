e462b51d63e9cc19be20b30b2943224501209453
Nick Payne <nick.payne89@gmail.com>
2019-03-07 14:05:45 +0800

    system properties gae

--- tournamator-app/src/main/kotlin/ch/tournamator/App.kt
+++ tournamator-app/src/main/kotlin/ch/tournamator/App.kt
@@ -2,6 +2,7 @@ package ch.tournamator
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.google.appengine.api.datastore.*
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
@@ -12,7 +13,30 @@ import org.springframework.context.annotation.Configuration
 class App
 
 fun main(args: Array<String>) {
+
+    val profile = System.getProperty("spring.profiles.active")
+
+    if (profile == "prod") {
+        setEnvironmentPropertiesOffGae()
+    }
+
     runApplication<App>(*args)
+}
+
+fun setEnvironmentPropertiesOffGae() {
+    val datastoreService = DatastoreServiceFactory.getDatastoreService()
+
+    val q = Query("settings")
+
+    println("Looking for mongodb URI...")
+
+    val mongodbUri: Entity = datastoreService.prepare(q).asIterable()
+        .find { it.getProperty("GCLOUD_MONGODB_URI") != null } ?: return
+
+    println("Found mongodb URI...")
+
+    val property = mongodbUri.getProperty("MONGODB_URI")
+    System.setProperty("MONGODB_URI", property.toString())
 }
 
 @Bean
